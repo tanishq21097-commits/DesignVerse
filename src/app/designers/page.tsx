b6cbb2f756a1ccb1d227e70529481f4d58c21f32
Tanishq Kumar <tanishq2521@gmail.com>
2026-01-30 16:54:47 +0000

    Try fixing this error: `Runtime ReferenceError: CardHeader is not define

--- src/app/designers/page.tsx
+++ src/app/designers/page.tsx
@@ -1,8 +1,11 @@
+
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card"
 import {
   DropdownMenu,
